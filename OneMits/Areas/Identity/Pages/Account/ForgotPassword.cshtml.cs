f13eeca0b073a1e6996d8e20373cf057bb911e0c
Nishant Raghuvanshi <nishant4337@outlook.com>
2019-10-02 00:42:54 +0530

    OTP Dekh

--- OneMits/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
+++ OneMits/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
@@ -44,7 +44,7 @@ namespace OneMits.Areas.Identity.Pages.Account
                     // Don't reveal that the user does not exist or is not confirmed
                     return RedirectToPage("./ForgotPasswordConfirmation");
                 }
-
+                 
                 // For more information on how to enable account confirmation and password reset please 
                 // visit https://go.microsoft.com/fwlink/?LinkID=532713
                 var code = await _userManager.GeneratePasswordResetTokenAsync(user);
--- OneMits/Controllers/OTPController.cs
+++ OneMits/Controllers/OTPController.cs
@@ -1,31 +1,96 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using OneMits.Data;
 using OneMits.Data.Models;
-using OneMits.Views.OTP;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Authorization;
+using System.Threading.Tasks;
+using System.Text.Encodings.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlClient;
+using System;
+using System.Data;
 
 namespace OneMits.Controllers
 {
+    [AllowAnonymous]
     public class OTPController : Controller
     {
-        private readonly UserManager<ApplicationUser> _profileManager;
-        private readonly IApplicationUser _profileImplementation;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IEmailSender _emailSender;
 
-        public OTPController(UserManager<ApplicationUser> profileManager, IApplicationUser profileImplementation)
+
+        public OTPController(UserManager<ApplicationUser> userManager, IEmailSender emailSender)
         {
-            _profileManager = profileManager;
-            _profileImplementation = profileImplementation;
+            _userManager = userManager;
+            _emailSender = emailSender;
         }
 
-        public IActionResult Index()
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
         {
-           
-            return View();
+            [Required]
+            [EmailAddress]
+            public string User { get; set; }
         }
-      
+
+        protected void SearchByTagButton_Click(object sender, EventArgs e)
+        {
+
+            String strConn = @"Server=(localdb)\\mssqllocaldb;Database=OneMitsDatabase;Trusted_Connection=True;MultipleActiveResultSets=true";
+            SqlConnection conn = new SqlConnection(strConn);
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("Select * FROM Patients WHERE Admin_ID = @admin_id and Admin_Password = @admin_password ", conn);
+            try
+            {
+
+                SqlParameter search = new SqlParameter();
+                search.ParameterName = "@admin_id";
+                search.ParameterName = "@admin_password";
+                search.Value = SearchByTagTB.Text.Trim();
+
+                cmd.Parameters.Add(search);
+                SqlDataReader dr = cmd.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(dr);
+            }
+            catch (Exception)
+            {
+
+            }
+            conn.Close();
+
+        }
+
+        public async Task<IActionResult> OTPAsync()
+        {
+            var user = await _userManager.FindByNameAsync(Input.User);
+            if (user == null || !(await _userManager.IsEmailConfirmedAsync(user)))
+            {
+                // Don't reveal that the user does not exist or is not confirmed
+                return RedirectToPage("./ForgotPasswordConfirmation");
+            
+            }
+
+            var Email1 = user.Email; 
+            var code = await _userManager.GeneratePasswordResetTokenAsync(user);
+            var callbackUrl = Url.Page(
+                "/Account/ResetPassword",
+                pageHandler: null,
+                values: new { code },
+                protocol: Request.Scheme);
+
+            await _emailSender.SendEmailAsync(
+                Email1,
+                "Reset Password",
+                $"Click Here For MAke Password <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>Link</a>.");
+
+            return RedirectToPage("./Not_Matched");
+        }
+
+            return Page();
+        
+
     }
 }
